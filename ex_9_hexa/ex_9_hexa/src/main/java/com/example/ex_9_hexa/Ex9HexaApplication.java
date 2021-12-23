177b1844cc03e771d7b01f78d2f06ddabce3ac85
Paloma Rebolleda <paloma.rebolleda@gmail.com>
2021-12-23 12:37:17 +0100

    start hexa proyect

---
+++ ex_9_hexa/ex_9_hexa/src/main/java/com/example/ex_9_hexa/Ex9HexaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex_9_hexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex9HexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex9HexaApplication.class, args);
+	}
+
+}
---
+++ ex_9_hexa/ex_9_hexa/src/test/java/com/example/ex_9_hexa/Ex9HexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex_9_hexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex9HexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

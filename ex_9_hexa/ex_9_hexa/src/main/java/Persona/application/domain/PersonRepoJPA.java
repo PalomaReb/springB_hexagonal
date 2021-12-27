e0c796783edf75d89d33ad60706cf5998274ad16
Paloma Rebolleda <paloma.rebolleda@gmail.com>
2021-12-27 11:34:55 +0100

    not null

--- ex_9_hexa/ex_9_hexa/src/main/java/Persona/application/domain/PersonRepoJPA.java
+++ ex_9_hexa/ex_9_hexa/src/main/java/Persona/application/domain/PersonRepoJPA.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PersonRepoJPA extends JpaRepository<Person, Integer> {
-    default List<Person> findByName(String name) {
-        return null;
-    }
+    List<Person> findByName(String name);
 
 }
